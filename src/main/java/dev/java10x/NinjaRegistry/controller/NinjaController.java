551e4c38948ddb2894dcadd46d01b14556e47623
famantea <amanteaferreira@gmail.com>
2026-09-22 22:20:14 -0300

    Adding JPA dependency + Create Ninja Table

--- src/main/java/dev/java10x/NinjaRegistry/controller/NinjaController.java
+++ src/main/java/dev/java10x/NinjaRegistry/controller/NinjaController.java
@@ -1,19 +1,12 @@
 package dev.java10x.NinjaRegistry.controller;
 
-import dto.Ninja;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/ninjas")
 public class NinjaController {
 
-    @PostMapping
-    public Ninja createNinja(@RequestBody Ninja ninja) {
-        return ninja;
-    }
-
     @GetMapping("/{id}")
-    public Ninja getNinjaById(@PathVariable String id) {
-        return new Ninja();
+    public void getNinjaById(@PathVariable String id) {
     }
 }
